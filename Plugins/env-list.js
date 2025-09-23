758607aecc803be273ccf2a5f423bd5083d5e453
YOUNGFABOE456 <youngfaboe456@gmail.com>
2025-09-23 13:51:49 +0300

    Update env-list.js

--- Plugins/env-list.js
+++ Plugins/env-list.js
@@ -55,7 +55,7 @@ async (conn, mek, m, { from, quoted, reply }) => {
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
                         newsletterJid: '120363315949714553@newsletter',
-                        newsletterName: '𝚫𝚴𝐃𝚩𝚫𝐃-𝚳𝐃-𝛁2',
+                        newsletterName: 'YOUNGFABOE-X𝚳𝐃-𝛁456',
                         serverMessageId: 143
                     }
                 }
