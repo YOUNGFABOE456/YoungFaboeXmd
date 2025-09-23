bffca0f4ec1cd89a61db115125473f77300a7ba1
YOUNGFABOE456 <youngfaboe456@gmail.com>
2025-09-23 16:09:35 +0300

    Update weather.js

--- Plugins/weather.js
+++ Plugins/weather.js
@@ -28,7 +28,7 @@ async (conn, mek, m, { from, q, reply }) => {
 > 💨 *Wind Speed*: ${data.wind.speed} m/s
 > 🔽 *Pressure*: ${data.main.pressure} hPa
 
-> *© Powdered By CRISS AI
+> *© Powdered By YOUNGFABOE XMD 
 `;
         return reply(weather);
     } catch (e) {
